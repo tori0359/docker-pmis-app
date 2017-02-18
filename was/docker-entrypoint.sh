cf2d90898f1def96e9b588af3f9a5fbf8145bb8f
Emanuele Disco <emawind84@gmail.com>
2017-02-19 00:42:36 +0900

    update

--- was/docker-entrypoint.sh
+++ was/docker-entrypoint.sh
@@ -2,13 +2,20 @@
 
 set -e
 
+export WEBAPP_BASE=/usr/local/webapp
+
 if [ -f "/usr/local/src/stnd_pmis.war" ]; then
-    rm -rf /usr/local/pmis/ROOT
-    mkdir -p /usr/local/pmis && mv -f /usr/local/src/stnd_pmis.war /usr/local/pmis/pmis.war
+    rm -rf $WEBAPP_BASE/ROOT
+    mkdir -p $WEBAPP_BASE && mv -f /usr/local/src/stnd_pmis.war $WEBAPP_BASE/pmis.war
 fi
 
 JVM_ROUTE=${JVM_ROUTE:-worker1}
 sed -i 's/worker1/$JVM_ROUTE/' /usr/local/tomcat/conf/server.xml
+
+export JAVA_OPTS="$JAVA_OPTS \
+-Ddb.Url=\"$DB_URL\" \
+-Ddb.Username=$DB_USERNAME \
+-Ddb.Password=$DB_PASSWORD"
 
 if [ $# -eq 0 ]; then
     # if no arguments are supplied start apache
