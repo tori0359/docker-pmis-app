58b22520ab0682de85fdf788530387203ac44a2e
Emanuele Disco <emawind84@gmail.com>
2017-02-23 10:20:13 +0900

    update

--- docker-auto.sh
+++ docker-auto.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+PROJECT_NAME="$PROJECT_NAME"
+if [ -z "$PROJECT_NAME" ]; then
+    PROJECT_NAME="$(cat .env | awk 'BEGIN { FS="="; } /^PROJECT_NAME/ {sub(/\r/,"",$2); print $2;}')"
+fi
+REGISTRY_URL="$REGISTRY_URL"
+if [ -z "$REGISTRY_URL" ]; then
+    REGISTRY_URL="$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')"
+fi
+
 SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPT_BASE_PATH"
 
@@ -26,6 +35,8 @@ echo "  ps              Show the status of the services"
 echo "  logs            Follow the logs on console"
 echo "  remove-all      Remove all containers"
 echo "  stop-all        Stop all containers running"
+echo "  build           Build the image"
+echo "  publish         Publish the image to the registry"
 }
 
 CONF_ARG=""
@@ -79,14 +90,32 @@ if [ "$1" == "up" ]; then
     docker-compose $CONF_ARG build --pull
     docker-compose $CONF_ARG up -d
     exit 0
+
 elif [ "$1" == "stop-all" ] && [ "$(docker ps --format {{.ID}})" != ""]; then
     docker stop $(docker ps --format {{.ID}})
     exit 0
+
 elif [ "$1" == "remove-all" ] && [ "$(docker ps -a --format {{.ID}})" != "" ]; then
     docker rm $(docker ps -a --format {{.ID}})
     exit 0
+
 elif [ "$1" == "logs" ]; then
     docker-compose $CONF_ARG logs -f --tail 200
+    exit 0
+
+elif [ "$1" == "build" ]; then
+    if [ -z "$REGISTRY_URL" ]; then echo "REGISTRY_URL not defined."; exit 1; fi
+    if [ -z "$PROJECT_NAME" ]; then echo "PROJECT_NAME not defined."; exit 1; fi
+
+    docker build -t $REGISTRY_URL/$PROJECT_NAME was
+    exit 0
+
+elif [ "$1" == "publish" ]; then
+    if [ -z "$REGISTRY_URL" ]; then echo "REGISTRY_URL not defined."; exit 1; fi
+    if [ -z "$PROJECT_NAME" ]; then echo "PROJECT_NAME not defined."; exit 1; fi
+    
+    docker login $REGISTRY_URL
+    docker push $REGISTRY_URL/$PROJECT_NAME
     exit 0
 fi
 
